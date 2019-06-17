af6ab97ed7d400475170bea65c71540991527fbb
Hoxtygen <hoxtygen@live.com>
2019-06-17 14:12:39 +0100

    Implement counter increment
    The counter increment functionality was implemented, and when the button for increasing the counter is clicked the counter increases in value

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,7 +13,7 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
-        state: state.count + 1
+        count: state.count + 1
       }
     // Fill in the body of this case
     case DECREMENT:
